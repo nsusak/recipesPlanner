c2eb9b609f629083cce0177f7d9d6899f674e9e3
nikolina.susak <nikolina.susak@leapbit.com>
2021-12-10 13:03:06 +0100

    units route

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,7 @@ import usersRoutes from './routes/users.js';
 import ingredientsRoutes from './routes/ingredients.js';
 import recipesRoutes from './routes/recipes.js';
 
+import { db } from '../backend/db/connection.js';
 initConnection();
 
 const PORT = 5000;
@@ -22,5 +23,13 @@ app.use('/recipes', recipesRoutes);
 
 app.get('/', (req, res) => res.send('Hello from Home page'));
 
+app.get('/units', (req, res) => {
+    const sql = `SELECT * FROM measuring_units`;
+
+    db.query(sql, (err, results) => {
+        if (err) throw err;
+        res.send(results);
+    });
+});
 
 app.listen(PORT, () => console.log('Server started on port', PORT));
